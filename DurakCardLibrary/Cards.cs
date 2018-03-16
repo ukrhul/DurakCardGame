1cf7fd439d5d6ba24940720a44e01cb8f04ec694
Rahul <ukrhul@gmail.com>
2018-03-16 15:33:15 -0400

    Class Diagram edited, Deck header edited

--- DurakCardLibrary/Cards.cs
+++ DurakCardLibrary/Cards.cs
@@ -6,7 +6,7 @@
 *	copyTo, contains and clone method
 *	for cards.
 *   
-*	Author		  Rahul
+*	Author		  Rahul, Jaspreet kaur, Remya Zacharias 
 *	Since		  1.0 (25 Feb 2018)
 *	Version       2.0 (11 March 2018)
 *	Reference     Chapter 12 Example Application Page 295 From Beginning Visual C# 2012 
@@ -20,41 +20,6 @@ namespace Ch11CardLib
 {
     public class Cards: List<Card>, ICloneable
     {
-        ///// <summary>
-        ///// Method to add the card to the collection list
-        ///// </summary>
-        ///// <param name="newCard">parameter for the card</param>
-        //public void Add(Card newCard)
-        //{
-        //    List.Add(newCard);  //Add the card to the collection list
-        //}
-
-        ///// <summary>
-        ///// Method to remove the card from the collection list
-        ///// </summary>
-        ///// <param name="oldCard"></param>
-        //public void Remove(Card oldCard)
-        //{
-        //    List.Remove(oldCard);  //Remove the card from the collection list
-        //}
-
-        ///// <summary>
-        ///// Property to get and set the card
-        ///// </summary>
-        ///// <param name="cardIndex">index of the card</param>
-        ///// <returns>Card value</returns>
-        //public Card this[int cardIndex]
-        //{
-        //    get
-        //    {
-        //        return (Card)List[cardIndex];       //return the card from list
-        //    }
-        //    set
-        //    {
-        //        List[cardIndex] = value;            //set the value to list at card index
-        //    }
-        //}
-
         /// <summary>
         /// CopyTo method to copy the cards
         /// </summary>
@@ -67,16 +32,6 @@ namespace Ch11CardLib
                 targetCards[index] = this[index];       //assgin card index to target card index
             }
         }
-
-        ///// <summary>
-        ///// Contains method to check collection contains card or not
-        ///// </summary>
-        ///// <param name="card"></param>
-        ///// <returns>Boolean value</returns>
-        //public bool Contains(Card card)
-        //{
-        //    return InnerList.Contains(card);    //return true or false
-        //}
 
         /// <summary>
         /// Clone method to copy the cards(Deck) 
